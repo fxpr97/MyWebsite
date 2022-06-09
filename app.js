8ddb4475926e8710b0e4025b1fea10e22d375322
Francisco Pomaya-Ramos <franxavier1997@hotmail.com>
2022-06-08 20:21:55 -0400

    Added Scroll Smoothness

--- app.js
+++ app.js
@@ -1,10 +1,64 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar_menu')
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar_menu');
+
+const navLogo = document.querySelector('#navbar_logo');
 
 // Display Mobile Menu
 const mobileMenu = () => {
-  menu.classList.toggle('is-active')
-  menuLinks.classList.toggle('active')
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
 }
 
 menu.addEventListener('click', mobileMenu);
+
+// # for id
+// . for classes
+
+// Show active menu when scrolling
+const highlightMenu = () => {
+  const elem = document.querySelector('.highlight');
+  const homeMenu = document.querySelector('#home-page');
+  const aboutMenu = document.querySelector('#about-page');
+  const servicesMenu = document.querySelector('#services-page');
+
+  // Position on the site
+  let scrollPos = window.scrollY;
+
+  // adds 'highlight' class to my menu items
+  if (window.innerWidth > 960 && scrollPos < 500) {
+    homeMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+    return;
+  }
+  else if (window.innerWidth > 960 && scrollPos < 1100) {
+    aboutMenu.classList.add('highlight');
+    homeMenu.classList.remove('highlight');
+    servicesMenu.classList.remove('highlight');
+    return;
+  }
+  else if (window.innerWidth > 960 && scrollPos < 1700) {
+    servicesMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+    return;
+  }
+
+  if ((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
+    elem.classList.remove('highlight');
+  }
+}
+
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
+
+// Close mobile Menu when clicking on a menu item
+const hideMobileMenu = () => {
+  const menuBars = document.querySelector('.is-active');
+
+  if (window.innerWidth <= 960 && menuBars) {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.remove('active');
+  }
+}
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
